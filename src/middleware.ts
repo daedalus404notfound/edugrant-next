04fb4fa01bf94e3830dd5a681f5eee9d603ca145
Neal_Eleazar_J_Ramos <nealramos72@gmail.com>
2025-07-12 19:41:54 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,19 +4,23 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(request: NextRequest){
     const { pathname } = request.nextUrl;
     if(pathname.startsWith("/administrator/home")){
-        const ValidToken = await fetch(`${process.env.NEXT_PUBLIC_USER_API}/adminTokenAuthentication`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            cookie: request.headers.get("cookie") || "", // Forward user's cookies
-        },
-        body: JSON.stringify({}),
-        });
-        if(ValidToken.status !== 200){
-            return NextResponse.redirect(new URL("/administrator", request.url))
-        }
-        else{
-            return NextResponse.next()
+        try {
+            const ValidToken = await fetch(`${process.env.NEXT_PUBLIC_USER_API}/adminTokenAuthentication`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                cookie: request.headers.get("cookie") || "", // Forward user's cookies
+            },
+            body: JSON.stringify({}),
+            });
+            if(ValidToken.status !== 200){
+                return NextResponse.redirect(new URL("/administrator", request.url))
+            }
+            else{
+                return NextResponse.next()
+            }
+        } catch (error) {
+            console.log(error)
         }
     }
 }
