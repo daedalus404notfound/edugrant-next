dd93137db1d364829a738d16d1528617f5790804
Neal_Eleazar_J_Ramos <nealramos72@gmail.com>
2025-07-12 19:25:03 +0800

    Create middleware.ts

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function middleware(request: NextRequest){
+    const { pathname } = request.nextUrl;
+    if(pathname.startsWith("/administrator/home")){
+        const ValidToken = await fetch(`${process.env.NEXT_PUBLIC_USER_API}/adminTokenAuthentication`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            cookie: request.headers.get("cookie") || "", // Forward user's cookies
+        },
+        body: JSON.stringify({}),
+        });
+        if(ValidToken.status === 200){
+            return NextResponse.redirect(new URL("/administrator", request.url))
+        }
+    }
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher:"/administrator/home/:path*",
+}
