f1e5feaf12fd3e59cd511b759cd9db2e2e23424e
iamDaedalus <jerometecson.basc@gmail.com>
2025-09-14 15:33:53 +0800

    2322

--- src/components/head-sidebar/nav-user.tsx
+++ src/components/head-sidebar/nav-user.tsx
@@ -32,12 +32,12 @@ export function NavUser() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={admin?.profileImage} alt={admin?.firstName} />
+                <AvatarImage src={``} alt={admin?.ISPSU_Head.fName} />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">
-                  Admin {admin?.firstName}
+                  Admin {admin?.ISPSU_Head.fName}
                 </span>
                 <span className="truncate text-xs">{admin?.email}</span>
               </div>
@@ -53,15 +53,13 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage
-                    src={admin?.profileImage}
-                    alt={admin?.firstName}
-                  />
+                  <AvatarImage src={``} alt={admin?.ISPSU_Head.fName} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">
-                    Admin {admin?.firstName}
+                    {admin?.ISPSU_Head.fName}
+                    {admin?.ISPSU_Head.lName}
                   </span>
                   <span className="truncate text-xs">{admin?.email}</span>
                 </div>
