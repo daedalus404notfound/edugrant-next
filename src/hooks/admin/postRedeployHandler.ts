eea23a55f1dc81241f3b2b1c2011a44e9951a037
iamDaedalus <jerometecson.basc@gmail.com>
2025-09-19 23:23:19 +0800

    .

--- src/hooks/admin/postRedeployHandler.ts
+++ src/hooks/admin/postRedeployHandler.ts
@@ -19,8 +19,8 @@ const addScholarshipApi = async (data: renewDocumentsFormData) => {
     `${process.env.NEXT_PUBLIC_ADMINISTRATOR_URL}/renewScholarship`,
     {
       renewDeadline: data.renewDeadline.toISOString(),
-      scholarshipId: data.scholarshipId ?? null,
-      accountId: data.accountId ?? null,
+      scholarshipId: data.scholarshipId,
+      accountId: data.accountId,
       renewDocuments: data.renewDocuments,
     },
     {
