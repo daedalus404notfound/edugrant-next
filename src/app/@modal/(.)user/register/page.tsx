a80a4ae5f8ba8a58df5f8634ad6c73586e2374bc
iamDaedalus <jerometecson.basc@gmail.com>
2025-09-28 04:33:36 +0800

    .

--- src/app/@modal/(.)user/register/page.tsx
+++ src/app/@modal/(.)user/register/page.tsx
@@ -184,9 +184,9 @@ export default function RegisterStudent() {
             <Form {...personalForm}>
               <div className="space-y-6">
                 <motion.div
-                  initial={{ opacity: 0, x: -30 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{ duration: 0.6, delay: 0.3 }}
+                  initial={{ opacity: 0, y: 30 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.6, delay: 0.2 }}
                   className="lg:pt-8 lg:px-8 pt-6 px-2"
                 >
                   <h1 className="text-lg font-semibold">
