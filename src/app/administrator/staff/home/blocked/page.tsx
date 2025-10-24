f38cbded63591cc5ea41f02b416f0fc826d91dc3
iamDaedalus <jerometecson.basc@gmail.com>
2025-10-25 01:53:45 +0800

    3232

--- src/app/administrator/staff/home/blocked/page.tsx
+++ src/app/administrator/staff/home/blocked/page.tsx
@@ -1,30 +1,73 @@
 "use client";
 import "ldrs/react/Ring.css";
-import { UserRoundMinus } from "lucide-react";
 import { useState } from "react";
-import { TourProvider } from "@/components/tour/tour-provider";
-import { TourStep } from "@/components/tour/tour-step";
-import type { TourStep as TourStepType } from "@/lib/use-tour";
+import { DataTable } from "@/app/table-components/data-table";
+import { columns } from "../staff-application-table-components/columns";
+import {
+  ColumnFiltersState,
+  PaginationState,
+  SortingState,
+} from "@tanstack/react-table";
+import DataTableToolbar from "../staff-application-table-components/data-table-toolbar";
+import { ApplicationFormData } from "@/hooks/zod/application";
+import useApplicationDataStaff from "@/hooks/staff/getApplicationStaff";
 import TitleReusable from "@/components/ui/title";
-import { Tabs } from "@/components/ui/vercel-tabs";
-import ApprovedApplication from "./blocked";
 
-export default function BlockedApplication() {
-
-
+export default function PendingStaffApplication() {
+  const [columnVisibility, setColumnVisibility] = useState<
+    Record<string, boolean>
+  >({
+    phase: false,
+    section: false,
+    year: false,
+    institute: false,
+  });
+  const [search, setSearch] = useState("");
+  const [status, setStatus] = useState("BLOCKED");
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const { data, meta, isLoading } = useApplicationDataStaff({
+    pagination,
+    sorting,
+    columnFilters,
+    status,
+    search,
+  });
 
   return (
     <div className="lg:px-4 lg:min-h-[calc(100vh-80px)] min-h-[calc(100dvh-134px)] ">
       <div className="mx-auto lg:w-[95%]  w-[95%] py-10">
         <TitleReusable
-          textColor="text-emerald-700/70"
-          title="Approved Applications"
-          description="Applications that have been approved and finalized."
-          Icon={UserRoundMinus}
+          title="Pending Applications"
+          textColor="text-yellow-700/70"
+          description="Applicants currently waiting for review."
         />
 
         <div className="py-8 space-y-5">
-          <ApprovedApplication />
+          <DataTable<ApplicationFormData, unknown>
+            data={data}
+            columns={columns}
+            meta={meta}
+            pagination={pagination}
+            setPagination={setPagination}
+            getRowId={(row) => row.scholarshipId}
+            loading={isLoading}
+            search={search}
+            setSearch={setSearch}
+            status={status}
+            setStatus={setStatus}
+            sorting={sorting}
+            setSorting={setSorting}
+            columnFilters={columnFilters}
+            setColumnFilters={setColumnFilters}
+            toolbar={DataTableToolbar}
+            columnVisibility={columnVisibility} // <-- pass visibility
+            setColumnVisibility={setColumnVisibility} // <-- pass setter
+          />
         </div>
       </div>
     </div>
